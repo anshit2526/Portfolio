12c442a25fa847a62f6d1826f7da926187440d28
Anshit Krishan <anshitsiddharth2003@gmail.com>
2024-03-08 21:15:58 +0530

    Made contact form functional. Form can be use to contact me

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 
 export default function Contact() {
+
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "753bab28-a262-445d-b077-884825b3128c");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+
     return (
         <div className='p-10'>
             <div className='text-xl flex items-center text-my-theme mb-8'>Contact <span className='h-[1px] w-[35dvw] bg-my-theme mx-10 rounded-xl' > </span></div>
-                <form action="#" name='contact' id='contact' className='flex flex-col border border-my-theme rounded-lg py-2 px-7 w-[50vw] mx-auto'>
+                <form onSubmit={onSubmit} name='contact' id='contact' className='flex flex-col border border-my-theme rounded-lg py-2 px-7 w-[50vw] mx-auto'>
                     <label htmlFor="name">Name*</label>
                     <input type="text" name='name' id='name' required />
 
